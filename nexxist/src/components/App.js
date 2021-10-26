60ebaa8fa68d59fed7cd7b343f70e4700e4864a8
OmarJ <omarjandali93@gmail.com>
2021-10-26 11:30:38 -0700

    setup the header. had to disable the searching for google search because it wascausing an issues due to api key

--- nexxist/src/components/App.js
+++ nexxist/src/components/App.js
@@ -1,11 +1,42 @@
-import React from 'react'
-import Header from './Header.js'
-import '../css/app.css'
+import React, { useState } from 'react';
+import HeaderMobile from './Header.js';
+import '../css/app.css';
+
+export const HeaderContext = React.createContext()
 
 export default function App() {
+  const [searchSelected, setSearchSelected] = useState(false)
+  const [address, setAddress] = useState('')
+
+  const handleSelectedAddress = async () => {
+    console.log('selectedAddress')
+  }
+
+  const headerContextValue = {
+    handleSearchSelectedOn,
+    handleSearchSelectedOff,
+    handleAddressChange,
+    handleSelectedAddress
+  }
+
+  function handleSearchSelectedOn(){
+    setSearchSelected(true)
+  }
+
+  function handleSearchSelectedOff(){
+    setSearchSelected(false)
+  }
+
+  function handleAddressChange(e){
+    console.log(e)
+    // setSearchedAddress(e)
+  }
+
   return (
     <div>
-      <Header />
+      <HeaderContext.Provider value={headerContextValue}>
+        <HeaderMobile searchSelected={searchSelected} address={address} setAddress={setAddress}/>
+      </HeaderContext.Provider>
     </div>
   )
 }
--- nexxist/src/components/Header.js
+++ nexxist/src/components/Header.js
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import logo from '../images/nexxist.png'
 import search from '../images/loupe.png'
 import like from '../images/like.png'
 import user from '../images/user.png'
+import { HeaderContext } from './App.js'
 
-export default function Header() {
+export default function HeaderMobile(props) {
+  const { handleSearchSelectedOn } = useContext(HeaderContext)
   return (
-    <div className="header-container container-section">
-      {/* <p>hellow world</p> */}
-      <img className="logo" alt="Nexxist Logo" src={logo}/>
-      <div>
-        <img className="header-image" alt="Search Image" src={search}/>
-        <img className="header-image" alt="Like Image" src={like}/>
-        <img className="header-image" alt="User Image" src={user}/>
+    <>
+      <div className="header-container container-section">
+        <img className="logo" alt="Nexxist Logo" src={logo}/>
+        <div>
+        <img onClick={() => {handleSearchSelectedOn()}} className="header-image" alt="Search" src={search}/>
+          <img className="header-image" alt="Like " src={like}/>
+          <img className="header-image" alt="User " src={user}/>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
