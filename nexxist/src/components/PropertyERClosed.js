4996593acc02974c623bb9d750338be91871fcc2
OmarJ <omarjandali93@gmail.com>
2021-10-28 09:24:23 -0700

    restructured the expenses section to show expenses in a more structured way

--- nexxist/src/components/PropertyERClosed.js
+++ nexxist/src/components/PropertyERClosed.js
@@ -7,20 +7,58 @@ export default function PropertyERClosed(props) {
   } = props;
 
   const { calculateMontlyExpenses } = useContext(PropertyContext)
+  const { calculateMonthlyMortgage } = useContext(PropertyContext)
   const { handlePropertyRevenueExpensesEdit } = useContext(PropertyContext)
 
   let totalMonthlyPayment = calculateMontlyExpenses(property)
+  let monthlyMortgage = calculateMonthlyMortgage(property.price)
 
   return (
     <>
       <div className="expenses-container">
         <p>Monthly Costs</p>
-        <button onClick={() => {handlePropertyRevenueExpensesEdit(property.id)}}>EDIT</button>
       </div>
-      <div>
+      <p>{totalMonthlyPayment}</p>
+      <div className="monthly-expense-container">
         <p>Principle &amp; Interest:</p>
-        <div>
-          
+        <div className="monthly-expense-value-conatiner">
+          <p>${monthlyMortgage.toFixed(2)}</p>
+          <button>Edit</button>
+        </div>
+      </div>
+      <div className="monthly-expense-container">
+        <p>Mortgage Insurance:</p>
+        <div className="monthly-expense-value-conatiner">
+          <p>${0}</p>
+          <button>Edit</button>
+        </div>
+      </div>
+      <div className="monthly-expense-container">
+        <p>Property Tax:</p>
+        <div className="monthly-expense-value-conatiner">
+          <p>${property.property_tax}</p>
+          <button>Edit</button>
+        </div>
+      </div>
+      <div className="monthly-expense-container">
+        <p>Home Insurance:</p>
+        <div className="monthly-expense-value-conatiner">
+          <p>${property.home_insurance}</p>
+          <button>Edit</button>
+        </div>
+      </div>
+      <div className="monthly-expense-container">
+        <p>HOA Fees:</p>
+        <div className="monthly-expense-value-conatiner">
+          <p>${property.hoa}</p>
+          <button>Edit</button>
+        </div>
+      </div>
+      <div className="monthly-expense-container">
+        <p>Additional Expenses:</p>
+        <div className="monthly-expense-value-conatiner">
+          <p>${0}</p>
+          <button>Edit</button>
         </div>
       </div>
       <div>
